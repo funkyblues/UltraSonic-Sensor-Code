d9bbea82155e828112e7ae12a0bfa2d9e9d7acdc
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-06-04 23:02:12 +0900

    Update test_final.py

--- test_final.py
+++ test_final.py
@@ -5,14 +5,30 @@ from datetime import datetime
 from datetime import date
 import pymysql
 
-
+# Arduino port
 port = serial.Serial("/dev/ttyACM0", "57600")
+
+# mysql settings
+dbServerIP = '211.214.111.12'
+dbUser = 'user'
+dbPW = 'ch100300'
+db = pymysql.connect(host=dbServerIP, port=3306, user=dbUser, passwd=dbPW, db='Dangsu', charset='utf8')
+cursor = db.cursor()
 
 now = datetime.now()
 start = date.today()
 start_mth = start.month
 start_day = start.day
 file_title = now.strftime('%Y-%m-%d')
+
+sql = f'''
+    CREATE TABLE `${file_title}` (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        time DATETIME NOT NULL,
+        data VARCHAR(100) NOT NULL,
+        PRIMARY KEY (id)
+    );
+'''
 
 while True:
     if ((start_day == date.today().day) and (start_mth == date.today().month)):
